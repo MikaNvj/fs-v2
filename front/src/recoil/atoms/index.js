6614287dcf9393d04ae96ae378ef0449e4f73fbc
Ludores <bejeryludores@gmail.com>
2023-07-15 09:02:07 +0300

    set export atoms into index

--- front/src/recoil/atoms/index.js
+++ front/src/recoil/atoms/index.js
@@ -1,11 +1,17 @@
-export * from './auth'
-export * from './user'
-export * from './connexion'
-export * from './payment'
-export * from './customer'
-export * from './income'
-export * from './program'
-export * from './formation'
-export * from './sub'
-export * from './copy'
-export * from './cert'
+import { authState } from "./auth";
+import { authState } from "./cert";
+import { connexionState } from "./connexion";
+import { copyState } from "./copy";
+import { customerState } from "./customer";
+import { formationState } from "./formation";
+import { icomeState } from "./income";
+import { payementState } from "./payement";
+import { programState } from "./program"; 
+import { subState } from "./sub";
+import { userState } from "./user";
+
+export default {
+    authState, authState, connexionState, copyState
+    , customerState, formationState, icomeState, payementState
+    , programState, subState, userState
+}
